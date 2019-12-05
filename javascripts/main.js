1118a78736ef8dd80f45f47a88e712eaa4e06489
Ike <ikescode@gmail.com>
2019-12-04 20:22:42 -0800

    [add] nicer-looking input with keyup search, rate limit protection, progress loader bar, colors

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,25 @@
-const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
 const resultsEl = document.getElementById("results");
+let i = 0
 
-searchForm.addEventListener("submit", function(e) {
-    e.preventDefault();
+// only use API shortly after user is finished typing (to prevent rate limiting)
+let timer = null;
+function keyDown() {
+    i = 0
+    startSlider();
+    clearTimeout(timer); 
+       timer = setTimeout(search, 1000)
+}
+
+function search() {
     const q = searchInput.value;
-    search(q);
-});
-
-function search(q) {
     const path = `http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`;
-
     fetch(path)
     .then(function(res) {
         return res.json();
     }).then(function(json) {
         console.log(json.data[0].images.fixed_width.url);
         let resultsHTML = "";
-
         json.data.forEach(function(obj) {
             const url = obj.images.fixed_width.url;
             const width = obj.images.fixed_width.width;
@@ -36,4 +38,23 @@ function search(q) {
     }).catch(function(err) {
         console.log(err.message);
     });
+    }
+
+
+
+function startSlider() {
+  if (i == 0) {
+    const elem = document.getElementById("loaderProgress");
+    const id = setInterval(frame, 10);
+    let width = 0;
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
 }
