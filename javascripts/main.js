ddfcb123bef46f0287fe72df28340f2ff2ac670d
Ike <ikescode@gmail.com>
2019-12-04 17:09:21 -0800

    [add] giphy search starter code && separate script and style from html

---
+++ javascripts/main.js
@@ -0,0 +1,39 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const resultsEl = document.getElementById("results");
+
+searchForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const q = searchInput.value;
+    search(q);
+});
+
+function search(q) {
+    const path = `http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`;
+
+    fetch(path)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        console.log(json.data[0].images.fixed_width.url);
+        let resultsHTML = "";
+
+        json.data.forEach(function(obj) {
+            const url = obj.images.fixed_width.url;
+            const width = obj.images.fixed_width.width;
+            const height = obj.images.fixed_width.height;
+            const title = obj.title;
+            resultsHTML += `<img
+            class="item"
+            src="${url}"
+            width="${width}"
+            height="${height}"
+            alt="${title}"
+            >`;
+        });
+
+        resultsEl.innerHTML = resultsHTML;
+    }).catch(function(err) {
+        console.log(err.message);
+    });
+}
